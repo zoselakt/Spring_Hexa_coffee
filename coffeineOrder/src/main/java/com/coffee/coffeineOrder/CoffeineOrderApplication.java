c702bc94621f026d001d1c8c1625fa74f70c2b71
zoselakt <95839182+zoselakt@users.noreply.github.com>
2023-02-18 14:35:25 +0900

    modify

---
+++ coffeineOrder/src/main/java/com/coffee/coffeineOrder/CoffeineOrderApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.coffeineOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeineOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeineOrderApplication.class, args);
+	}
+
+}
---
+++ coffeineOrder/src/test/java/com/coffee/coffeineOrder/CoffeineOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.coffeineOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeineOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
