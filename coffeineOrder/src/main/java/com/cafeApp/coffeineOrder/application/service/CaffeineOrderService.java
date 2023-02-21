d9d72008d860447b75a2efaba23595b6a0904598
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2023-02-21 23:47:44 +0900

    caffeineInPort 의존성 주입

--- coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/application/service/CaffeineOrderService.java
+++ coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/application/service/CaffeineOrderService.java
@@ -1,5 +1,6 @@
 package com.cafeApp.coffeineOrder.application.service;
 
+import com.cafeApp.coffeineOrder.application.port.out.CaffeineInPort;
 import org.springframework.stereotype.Service;
 
 import com.cafeApp.coffeineOrder.adapter.in.CaffeineOrderResponse;
@@ -8,7 +9,12 @@ import com.cafeApp.coffeineOrder.application.port.in.CaffeineOrderUserCase;
 import java.util.List;
 
 @Service
-public class CaffeineOrderService implements CaffeineOrderUserCase{
+public class CaffeineOrderService implements CaffeineOrderUserCase {
+	private final CaffeineInPort caffeineInPort;
+
+	public CaffeineOrderService(final CaffeineInPort caffeineInPort) {
+		this.caffeineInPort = caffeineInPort;
+	}
 
 	@Override
 	public List<CaffeineOrderResponse> listBy() {
