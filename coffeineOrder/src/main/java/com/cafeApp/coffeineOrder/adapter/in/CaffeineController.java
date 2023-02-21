40a65ec007ca5feaa7218daa8bc2a5f42bb8a2d9
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2023-02-21 23:25:28 +0900

    orderCaffeine 생성

--- coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/adapter/in/CaffeineController.java
+++ coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/adapter/in/CaffeineController.java
@@ -1,8 +1,14 @@
 package com.cafeApp.coffeineOrder.adapter.in;
 
+import com.cafeApp.coffeineOrder.application.port.in.CaffeineOrderUserCase;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CaffeineController {
 
+    @GetMapping("/order")
+    public CaffeineOrderResponse orderCaffeine() {
+        return null;
+    }
 }
