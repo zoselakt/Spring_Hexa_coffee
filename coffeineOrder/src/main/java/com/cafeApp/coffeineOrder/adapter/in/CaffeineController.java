905a1f9116d6ba0833f8e3ae7625cc59bd637bfc
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2023-02-21 23:28:07 +0900

    response return

--- coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/adapter/in/CaffeineController.java
+++ coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/adapter/in/CaffeineController.java
@@ -4,6 +4,8 @@ import com.cafeApp.coffeineOrder.application.port.in.CaffeineOrderUserCase;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CaffeineController {
     private final CaffeineOrderUserCase caffeineOrderUserCase;
@@ -13,7 +15,8 @@ public class CaffeineController {
     }
 
     @GetMapping("/order")
-    public CaffeineOrderResponse orderCaffeine() {
-        return null;
+    public List<CaffeineOrderResponse> orderCaffeine() {
+        final List<CaffeineOrderResponse> response = caffeineOrderUserCase.listBy();
+        return response;
     }
 }
