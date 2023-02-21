391be6f34e4eb59d9fe9fe57e2dd96c4cd26d40c
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2023-02-21 23:46:33 +0900

    rename: intoOrder -> listBy

--- coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/application/service/CaffeineOrderService.java
+++ coffeineOrder/src/main/java/com/cafeApp/coffeineOrder/application/service/CaffeineOrderService.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Service;
 import com.cafeApp.coffeineOrder.adapter.in.CaffeineOrderResponse;
 import com.cafeApp.coffeineOrder.application.port.in.CaffeineOrderUserCase;
 
+import java.util.List;
+
 @Service
 public class CaffeineOrderService implements CaffeineOrderUserCase{
 
 	@Override
-	public CaffeineOrderResponse intoOrder() {
+	public List<CaffeineOrderResponse> listBy() {
 		return null;
 	}
 }
